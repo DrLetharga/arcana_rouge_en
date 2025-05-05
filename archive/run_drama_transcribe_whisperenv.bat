fc512ac045e2118b3375520302ade87a4f261f75
DrLetharga <34651522+DrLetharga@users.noreply.github.com>
2025-05-05 08:56:21 -0400

    Delete archive directory

--- archive/run_drama_transcribe_whisperenv.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d %~dp0
-start cmd /k "tts\whisper-env\Scripts\python.exe tools\tools_drama_transcribe_faster_ascii_winfix2.py"
--- archive/run_transcription_fixed_path.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d %~dp0
-echo Running transcription with logging. Output will be saved to transcription_log.txt
-"C:\Users\Jason\AppData\Local\Programs\Python\Python310\python.exe" tools\tools_run_transcription_ascii.py >> transcription_log.txt 2>&1
-echo Done. Press any key to exit.
-pause >nul
